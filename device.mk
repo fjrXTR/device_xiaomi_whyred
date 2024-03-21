02294dce4fbdeea683bff5d5bd6f9b696c51e8c0
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    whyred: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- device.mk
+++ device.mk
@@ -463,13 +463,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.dual_role_usb
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
